c8762a1649679787b4dcbcc56b169ef95946aa9f
regerj <regerjacob@gmail.com>
2024-07-20 16:15:14 -0700

    Added lazygit plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -21,7 +21,26 @@ local plugins = {
         "rust-analyzer"
       }
     }
-  }
+  },
+  {
+    "kdheepak/lazygit.nvim",
+
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" }
+    },
+  },
 }
 return plugins
 
